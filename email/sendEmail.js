bc0a97721107aff0e8ead0a5f9147433d061ab06
AlexAntoine <antoinealex1234@gmial.com>
2022-09-11 16:15:53 -0400

    added emailing function

---
+++ email/sendEmail.js
@@ -0,0 +1,21 @@
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(HLG_KEY);
+
+const sendEmail = async(email, subject, message)=>{
+
+    const result = await sgMail.send({
+        to: 'hiddenleafgroup@gmail.com', // Change to your recipient
+        from: 'hlg@hiddenleafgroup.com', // Change to your verified sender
+        subject: `Subject:${email}- ${subject}`,
+        text: message,
+    });
+
+    console.log(result);
+
+}
+
+module.exports = sendEmail;
+
+
+
