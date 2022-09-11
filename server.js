abc54c9f2d7842f73d0f5a94f84195546b4b66a8
AlexAntoine <anotoinealex1234@gmail.com>
2022-09-11 03:31:57 -0400

    added page router middleware

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.listen(PORT,(req, res)=>{
     console.log(`Server is running on port ${PORT}`);
 });
 
+app.use(pageRouter)
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(pageRouter)
