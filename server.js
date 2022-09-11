567b9c0886076975879153524900537e2d837ecd
AlexAntoine <anotoinealex1234@gmail.com>
2022-09-11 02:28:11 -0400

    added new dependcy

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const path = require('path');
 const pageRouter = require('./routes/pages');
 const PORT = process.env.PORT || 3000
 const app = express();
+const bodyParser = require('body-parser')
 
 app.listen(PORT,(req, res)=>{
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.use(pageRouter)
-
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(pageRouter)
