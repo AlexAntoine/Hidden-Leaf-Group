66c51c3fe46ed5ac10f0a170562cbcb0b8da039f
AlexAntoine <anotoinealex1234@gmail.com>
2022-09-11 03:32:09 -0400

    might need this might not

---
+++ public/assets/js/email.js
@@ -0,0 +1,9 @@
+const name = document.getElementById('name');
+const form = document.getElementById('contactForm');
+
+// form.addEventListener('click',(e)=>{
+//     e.preventDefault();
+// })
+
+console.log(name); 
+console.log('hello')   
